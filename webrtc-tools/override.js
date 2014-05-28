2113ab021fee32bed056c72e9574255b49123433
André Susano Pinto <andresp@google.com>
2014-05-28 12:45:49 +0200

    Modified extension so it allows to override configuration and constraints when instantiating RTCPeerConnections.

--- webrtc-tools/override.js
+++ webrtc-tools/override.js
@@ -14,23 +14,63 @@
 // limitations under the License.
 //
 (function () {
+  // TODO(andresp): These settings should be settable runtime and not
+  // require modify the source code and reloading the extension.
+  var settings = {};
+
+  // If set overrides the application provided peer connection config.
+  settings.overridePcConfig = {
+    iceServers: [
+      { url: 'stun:stun.l.google.com:19302' },
+      {
+        username: '1401358751:156547625762562',
+        credential: '8SggXyp9uVrqfrWZ/1DpuBKdmq8=',
+        urls: [ 'turn:192.158.30.23:3478?transport=udp',
+                'turn:192.158.30.23:3479?transport=udp' ],
+      },
+    ]
+  };
+
+  // If set overrides the application provided peer connection constraints.
+  settings.overridePcConstraints = {
+    'optional': [
+      { 'googImprovedWifiBwe': true }
+    ]
+  };
+
+  // If true addIceCandidates will simulate non-relay candidates are added with
+  // success but never actually add them to the peer connection. When used on
+  // both peers on a connection leaves TURN as the only way to establish a
+  // connection.
+  //
+  // TODO(andresp): Switch to iceTransports constraint once Chrome supports it.
+  //                https://code.google.com/p/webrtc/issues/detail?id=1179
+  //
+  settings.ignoreAddNonRelayIceCandidates = true;
+
   var logPrefix = "WebRTC-tools > ";
-  console.log(logPrefix + "WebRTC Tools extension is running on this page.");
+  console.log(logPrefix + "WebRTC Tools extension is running on this page: " + JSON.stringify(settings));
 
   // Hijack window.webkitRTCPeerConnection.
   var webkitRTCPeerConnection_ = window.webkitRTCPeerConnection;
   window.webkitRTCPeerConnection = function (pcConfig, pcConstraints) {
+    if (typeof settings.overridePcConfig != 'undefined')
+      pcConfig = settings.overridePcConfig;
+
+    if (typeof settings.overridePcConstraints != 'undefined')
+      pcConstraints = settings.overridePcConstraints;
+
     var pc = new webkitRTCPeerConnection_(pcConfig, pcConstraints);
 
-    // Overwrite addIceCandidate such that non-relay candidates are never passed
-    // to the peer connection.
-    var addIceCandidate_ = pc.addIceCandidate.bind(pc);
-    pc.addIceCandidate = function(candidate, successCallback, failureCallback) {
-      if (candidate.candidate.indexOf("typ relay ") != -1) {
-        addIceCandidate_(candidate, successCallback, failureCallback);
-      } else {
-        console.log(logPrefix + "Dropping addIceCandidate of non relay type: " + candidate.candidate);
-        successCallback();
+    if (settings.ignoreAddNonRelayIceCandidates === true) {
+      var addIceCandidate_ = pc.addIceCandidate.bind(pc);
+      pc.addIceCandidate = function(candidate, successCallback, failureCallback) {
+        if (candidate.candidate.indexOf("typ relay ") == -1) {
+          console.log(logPrefix + "Dropping addIceCandidate of non relay type: " + candidate.candidate);
+          successCallback();
+        } else {
+          addIceCandidate_(candidate, successCallback, failureCallback);
+        }
       }
     }
 
